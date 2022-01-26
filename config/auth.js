dc78ba513bbbdd1b47474b74e0cd1c789486a1c2
Abdulrahman Jamal <aboodx@me.com>
2022-01-26 21:48:46 +0300

    deploying

--- config/auth.js
+++ config/auth.js
@@ -1,13 +1,3 @@
-
-// exports.ensureAuthenticated = (req, res, next) => {
-//     if(req.isAuthenticated()) {
-//         return next();
-//     }
-//     req.flash('error_msg' , 'please login to view this resource!');
-//     res.redirect('/users/login');
-// }
-
-
 exports.ensureAuthenticated = (req, res, next) => {
     if(req.isAuthenticated()) {
         return next();
